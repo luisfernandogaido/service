ed1a5d1225a896d4d988e5a1d19b236d0b2950ea
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2018-04-08 12:40:21 -0300

    Busca de órgãos: combinação de fulltext com leveinshtein

--- app/orgaos.go
+++ app/orgaos.go
@@ -7,6 +7,9 @@ import (
 	"encoding/json"
 	"sync"
 	"strings"
+
+	"github.com/arbovm/levenshtein"
+	"io/ioutil"
 )
 
 type resultadoBuscaOrgao struct {
@@ -21,6 +24,14 @@ type cacheOrgaos struct {
 
 var defaultCacheOrgaos = cacheOrgaos{
 	mapa: make(map[string][]modelo.Orgao),
+}
+
+var orgaos []modelo.Orgao
+
+func LoadOrgaos() error {
+	var err error
+	orgaos, err = modelo.Orgaos()
+	return err
 }
 
 func Orgs(w http.ResponseWriter, r *http.Request) {
@@ -52,6 +63,45 @@ func Orgs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-type", "application/json; charset=utf8")
 	fmt.Fprintln(w, string(bytes))
 }
+func MuitosOrgs(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Método não permitido.", http.StatusMethodNotAllowed)
+		return
+	}
+	defer r.Body.Close()
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var os []string
+	err = json.Unmarshal(bytes, &os)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rs := make([]resultadoBuscaOrgao, 0)
+	for _, o := range os {
+		orgs, err := selecionaOrgaos(o)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		r := resultadoBuscaOrgao{
+			Termo:     o,
+			Resultado: orgs,
+		}
+		rs = append(rs, r)
+	}
+	bytes, err = json.MarshalIndent(rs, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Add("Content-type", "application/json; charset=utf8")
+	fmt.Fprintln(w, string(bytes))
+
+}
 
 func selecionaOrgaos(txt string) ([]modelo.Orgao, error) {
 	txt = strings.ToLower(txt)
@@ -65,8 +115,28 @@ func selecionaOrgaos(txt string) ([]modelo.Orgao, error) {
 	if err != nil {
 		return nil, err
 	}
+	if len(orgs) == 0 {
+		orgs = selecionaOrgaosParecidos(txt)
+	}
 	defaultCacheOrgaos.mutex.Lock()
 	defaultCacheOrgaos.mapa[txt] = orgs
 	defaultCacheOrgaos.mutex.Unlock()
 	return orgs, nil
+}
+
+func selecionaOrgaosParecidos(txt string) []modelo.Orgao {
+	txt = strings.ToUpper(txt)
+	menorDis := 100
+	var proximos = make([]modelo.Orgao, 0)
+	for _, o := range orgaos {
+		d := levenshtein.Distance(txt, o.Nome)
+		if d < menorDis {
+			menorDis = d
+			proximos = proximos[:0]
+			proximos = append(proximos, o)
+		} else if d == menorDis {
+			proximos = append(proximos, o)
+		}
+	}
+	return proximos
 }
--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"service/app"
 	"net/http"
+	"log"
 )
 
 func main() {
+	if err := app.LoadOrgaos(); err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/orgaos", app.Orgs)
+	http.HandleFunc("/muitosorgaos", app.MuitosOrgs)
 	http.ListenAndServe(":4000", nil)
 }
-
