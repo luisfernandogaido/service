d3ebf0632574ba3d1cf3357a8568d39c06cd856b
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2018-04-27 16:28:01 -0300

    Conexão com mysql no docker.

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
-	"os"
 )
 
 var (
@@ -21,15 +20,16 @@ func init() {
 		}, "&",
 	)
 	var err error
-	host, err := os.Hostname()
+	//host, err := os.Hostname()
 	if err != nil {
 		panic(err)
 	}
-	if host == "Go" {
-		Ect, err = sql.Open("mysql", "root:1000sonhosreais@tcp(localhost:3306)/ect?"+params)
-	} else {
-		Ect, err = sql.Open("mysql", "root:Semaver13@tcp(localhost:3306)/ect?"+params)
-	}
+	Ect, err = sql.Open("mysql", "root:gogogo@tcp(mysqlserver:3306)/ect?"+params)
+	//if host == "Go" {
+	//	Ect, err = sql.Open("mysql", "root:1000sonhosreais@tcp(localhost:3306)/ect?"+params)
+	//} else {
+	//	Ect, err = sql.Open("mysql", "root:Semaver13@tcp(localhost:3306)/ect?"+params)
+	//}
 	if err != nil {
 		panic(err)
 	}
